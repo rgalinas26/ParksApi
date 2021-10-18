037cc00d03063d7d32fd1fa6ebc7d0f6bb256158
rgalinas26 <52674802+rgalinas26@users.noreply.github.com>
2021-10-18 15:15:44 -0400

    Initial commit

---
+++ ParksApi/Controllers/ParksController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParksApi.Controllers
+{
+    public class ParksController : ControllerBase
+    {
+        [HttpGet("/parks")]
+        public async Task<ActionResult<CollectionResult<ParkListItemModel>>> GetAllParks()
+        {
+            // go to the database and all that jazz
+            var response = new CollectionResult<ParkListItemModel>
+            {
+                Data = new List<ParkListItemModel>
+                {
+                    new ParkListItemModel{ Id= 1, Description = "Grant Park", Address="Mayfield Heights",OpenInformation ="Year Round, Closes at Dusk"},
+                    new ParkListItemModel { Id =2, Description = "Wilderness Preserve", Address ="Mentor", OpenInformation= "Year Round, Permit Needed"}
+                }
+            };
+            return Ok(response);
+        }
+    }
+
+    public class CollectionResult<T>
+    {
+        public IList<T> Data { get; set; }
+    }
+    public class ParkListItemModel
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string Address { get; set; }
+        public string OpenInformation { get; set; }
+    }
+}
